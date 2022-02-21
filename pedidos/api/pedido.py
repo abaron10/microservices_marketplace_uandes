026296599e616992a054e3f96cf97c3e0bcc5f83
Emilson D. Quintero P <e.quinterop@uniandes.edu.co>
2022-02-21 13:24:54 -0500

    se agrega patrón circuit breaker para cada llamado a los servicios

--- pedidos/api/pedido.py
+++ pedidos/api/pedido.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 from injector import inject
 import requests
 import json
 import os
-
-
+import pybreaker
 
 pedido_api = Blueprint('pedido_api', __name__)
 ORDERS_PATH = os.environ["ORDERS_PATH"]
@@ -12,64 +11,220 @@ AGENDA_PATH = os.environ["AGENDA_PATH"]
 PAYMENTS_PATH = os.environ["PAYMENTS_PATH"]
 SELLERS_PATH = os.environ["SELLERS_PATH"]
 MONOLITH_PATH = os.environ["MONOLITH_PATH"]
+FAILURES = 1
+TIMEOUT = 6
+
+circuitOrder = pybreaker.CircuitBreaker(fail_max=FAILURES, reset_timeout=TIMEOUT)
+circuitAgenda = pybreaker.CircuitBreaker(fail_max=FAILURES, reset_timeout=TIMEOUT)
+circuitPago = pybreaker.CircuitBreaker(fail_max=FAILURES, reset_timeout=TIMEOUT)
+circuitProveedor = pybreaker.CircuitBreaker(fail_max=FAILURES, reset_timeout=TIMEOUT)
+
+
+def create_order(data):
+    @circuitOrder
+    def create_order_post():
+        order_response = requests.post(f"{ORDERS_PATH}/orders", json=data, timeout=2)
+        if order_response.status_code == 201:
+            return 201,order_response
+        else:
+            return 503,order_response.json()
+    try:
+        return create_order_post()
+    except:
+        return 408,"TimeOut para el servicio de ordenes"
+
+def get_order(pedido_id):
+    @circuitOrder
+    def get_order_get():
+        order_response = requests.get(f"{ORDERS_PATH}/orders/{pedido_id}", timeout=2)
+        if order_response.status_code == 201:
+            return 201,order_response
+        else:
+            return order_response.status_code,order_response.json()
+    try:
+        return get_order_get()
+    except:
+        return 408,"TimeOut para el servicio de ordenes"
+
+def create_agenda(order_id, seller_id, agenda_data):
+    @circuitAgenda
+    def create_agenda_post():
+        agenda_response = requests.post(f"{AGENDA_PATH}/agenda/sellers/{seller_id}", json=agenda_data, timeout=2)
+        if agenda_response.status_code == 201:
+            return 201,agenda_response
+        else:
+            requests.delete(f"{ORDERS_PATH}/orders/{order_id}")
+            return 412,agenda_response.json()
+    try:
+        return create_agenda_post()
+    except:
+        requests.delete(f"{ORDERS_PATH}/orders/{order_id}")
+        return 408,"TimeOut para el servicio de agenda"
+
+def get_agenda(seller_id, pedido_id):
+    @circuitAgenda
+    def get_agenda_get():
+        agenda_response = requests.get(f"{AGENDA_PATH}/agenda/sellers/{seller_id}/order/{pedido_id}", timeout=2)
+        if agenda_response.status_code == 201:
+            return 201,agenda_response
+        else:
+            return agenda_response.status_code,agenda_response.json()
+    try:
+        return get_agenda_get()
+    except:
+        return 408,"TimeOut para el servicio de agenda"     
+
+def create_pago(order_id, seller_id, pago_data):
+    @circuitPago
+    def create_pago_post():
+        pago_response = requests.post(f"{PAYMENTS_PATH}/payments", json=pago_data, timeout=2)
+        if pago_response.status_code == 201:
+            return 201,pago_response
+        else:
+            requests.delete(f"{AGENDA_PATH}/agenda/sellers/{seller_id}/order/{order_id}")
+            requests.delete(f"{ORDERS_PATH}/orders/{order_id}")
+            return 412,pago_response.json()
+    try:
+        return create_pago_post()
+    except:
+        requests.delete(f"{AGENDA_PATH}/agenda/sellers/{seller_id}/order/{order_id}")
+        requests.delete(f"{ORDERS_PATH}/orders/{order_id}")
+        return 408,"TimeOut para el servicio de pagos"
+
+def get_pago(pedido_id):
+    @circuitPago
+    def get_pago_get():
+        pago_response = requests.get(f"{PAYMENTS_PATH}/payments/orders/{pedido_id}", timeout=2)
+        if pago_response.status_code == 201:
+            return 201,pago_response
+        else:
+            return pago_response.status_code,pago_response.json()
+    try:
+        return get_pago_get()
+    except:
+        return 408,"TimeOut para el servicio de pagos"
+
+def get_seller(seller_id):
+    @circuitProveedor
+    def get_seller_get():
+        seller_response = requests.get(f"{SELLERS_PATH}/sellers/{seller_id}", timeout=2)
+        if seller_response.status_code == 201:
+            return 201,seller_response
+        else:
+            return seller_response.status_code,seller_response.json()
+    try:
+        return get_seller_get()
+    except:
+        return 408,"TimeOut para el servicio de proveedores"
+
+def esta_abierto(circuit, name):
+    if circuit._state_storage.state == 'open':
+        print("Servicio de ", name, " estado del circuit0: ", circuit._state_storage.state)
+        return True,"Servicio de "+name+" temporalmente indisponible"
+    else:
+        print("Servicio de ", name, " estado del circuito: ", circuit._state_storage.state)
+        return False,""
 
 @inject
 @pedido_api.route('/', methods=['POST'])
 def crear_pedido():
+    #token = request.headers["x-auth-token"]
 
-    token = request.headers["x-auth-token"]
+    #validation_login = requests.get(f"{MONOLITH_PATH}/sesion/{token}")
 
-    validation_login = requests.get(f"{MONOLITH_PATH}/sesion/{token}")
-
-    if validation_login.status_code != 200:
-        return validation_login.json(), 401
+    #if validation_login.status_code != 200:
+    #    return validation_login.json(), 401
 
     data = request.get_json()
-    item_id = data["item"]["uuid"]
-    exist_product = requests.get(f"{MONOLITH_PATH}/producto/{item_id}")
+    #item_id = data["item"]["uuid"]
+    #exist_product = requests.get(f"{MONOLITH_PATH}/producto/{item_id}")
 
-    if exist_product.status_code != 200:
-        return exist_product.json(),404
+    #if exist_product.status_code != 200:
+    #    return exist_product.json(),404
 
-    order_response = requests.post(f"{ORDERS_PATH}/orders", json=data)
-
-    if order_response.status_code != 201:
-        return order_response.json(),503
+    cod_rta,order_response = create_order(data)
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitOrder, "ordenes")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{order_response}"}}', status=cod_rta, mimetype='application/json')
+        return order_response,cod_rta
 
     order_id = order_response.json()["orderId"]
     user_id = order_response.json()["userId"]
     seller_id = order_response.json()["sellerId"]
     agenda_data = { "uuid": order_id }
-    agenda_response = requests.post(f"{AGENDA_PATH}/agenda/sellers/{seller_id}", json=agenda_data)
-
-    if agenda_response.status_code != 201:
-        requests.delete(f"{ORDERS_PATH}/orders/{order_id}")
-        return agenda_response.json(),412
+    
+    cod_rta,agenda_response = create_agenda(order_id, seller_id, agenda_data)
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitAgenda, "agenda")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{agenda_response}"}}', status=cod_rta, mimetype='application/json')
+        return agenda_response,cod_rta
 
     pago_data = { "order": { "uuid": order_id }, "user": { "uuid": user_id } }
-    pago_response = requests.post(f"{PAYMENTS_PATH}/payments", json=pago_data)
+    cod_rta,pago_response = create_pago(order_id, seller_id, pago_data) 
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitPago, "pagos")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{pago_response}"}}', status=cod_rta, mimetype='application/json')
+        return pago_response,cod_rta   
 
-    if pago_response.status_code != 201:
-        requests.delete(f"{AGENDA_PATH}/agenda/sellers/{seller_id}/order/{order_id}")
-        requests.delete(f"{ORDERS_PATH}/orders/{order_id}")
-        return pago_response.json(),412
     print(f"{order_id}")
     return order_response.json(), 200
 
 @inject
 @pedido_api.route('/<pedido_id>', methods=['GET'])
 def dar_pedido(pedido_id: int):
-    token = request.headers["x-auth-token"] 
-    validation_login = requests.get(f"{MONOLITH_PATH}/sesion/{token}")
-    if validation_login.status_code != 200:
-        return validation_login.json(), 401
+    #token = request.headers["x-auth-token"] 
+    #validation_login = requests.get(f"{MONOLITH_PATH}/sesion/{token}")
+    #if validation_login.status_code != 200:
+    #    return validation_login.json(), 401
 
-    order_response = requests.get(f"{ORDERS_PATH}/orders/{pedido_id}")
+    cod_rta,order_response = get_order(pedido_id)
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitOrder, "ordenes")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{order_response}"}}', status=cod_rta, mimetype='application/json')
+        return order_response,cod_rta
+
     seller_id = order_response.json()["sellerId"]
     item_id = order_response.json()["itemId"]
-    agenda_response = requests.get(f"{AGENDA_PATH}/agenda/sellers/{seller_id}/order/{pedido_id}")
-    pago_response = requests.get(f"{PAYMENTS_PATH}/payments/orders/{pedido_id}")
-    seller_response = requests.get(f"{SELLERS_PATH}/sellers/{seller_id}")
+
+    cod_rta,agenda_response = get_agenda(seller_id, pedido_id) 
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitAgenda, "agenda")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{agenda_response}"}}', status=cod_rta, mimetype='application/json')
+        return agenda_response,cod_rta
+
+    cod_rta,pago_response = get_pago(pedido_id) 
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitPago, "pagos")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{pago_response}"}}', status=cod_rta, mimetype='application/json')
+        return pago_response,cod_rta 
+
+    cod_rta,seller_response = get_seller(seller_id)
+    if cod_rta != 201:
+        cbabierto,mensaje = esta_abierto(circuitProveedor, "proveedores")
+        if cbabierto:        
+            return Response(f'{{"mensaje":"{mensaje}"}}', status=503, mimetype='application/json')
+        if cod_rta == 408:
+            return Response(f'{{"mensaje":"{seller_response}"}}', status=cod_rta, mimetype='application/json')
+        return seller_response,cod_rta
+
     item_response = requests.get(f"{MONOLITH_PATH}/producto/{item_id}")
 
     agenda_json = agenda_response.json()
